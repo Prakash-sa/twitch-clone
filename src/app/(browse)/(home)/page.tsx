703ab5b697eb27cb6a0c5ecf5b735de80bb3da63
Prakash-sa <sainiprakash525@gmail.com>
2024-06-17 12:00:00 +0000

    Add page.tsx

---
+++ src/app/(browse)/(home)/page.tsx
@@ -0,0 +1,13 @@
+import { Suspense } from 'react';
+
+import { Results, ResultsSkeleton } from './_components/results';
+
+export default function Page() {
+  return (
+    <div className='mx-auto h-full max-w-screen-2xl p-8'>
+      <Suspense fallback={<ResultsSkeleton />}>
+        <Results />
+      </Suspense>
+    </div>
+  );
+}
